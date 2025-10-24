f8d78abe7beccb81fbb4da7e5ac05461e9070c88
tianchenhui <3090018282@qq.com>
2025-10-24 17:47:50 +0800

    一级路由配置+路由懒加载+组件分包

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from '@/App'
 import 'normalize.css'
+import '@/assets/css/index.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { RouteObject } from 'react-router-dom'
-import Discover from '@/views/discover'
+import React, { lazy } from 'react'
+import type { RouteObject } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+// import Discover from '@/views/discover'
+// import Mine from '@/views/mine'
+// import Focus from '@/views/focus'
+// import Download from '@/views/download'
+
+const Discover = lazy(() => import('@/views/discover'))
+const Mine = lazy(() => import('@/views/mine'))
+const Focus = lazy(() => import('@/views/focus'))
+const Download = lazy(() => import('@/views/download'))
 
 const routes: RouteObject[] = [
   {
+    path: '/',
+    element: <Navigate to="/discover" />
+  },
+  {
     path: '/discover',
     element: <Discover />
+  },
+  {
+    path: '/mine',
+    element: <Mine />
+  },
+  {
+    path: '/focus',
+    element: <Focus />
+  },
+  {
+    path: '/download',
+    element: <Download />
   }
 ]
 
